209f647e1438b38a2fec1fe52651115d1590ba67
Lev Kim <irene.kim@revature.net>
2020-10-29 17:25:32 -0400

    customized fonts, colors, and modified homepage

--- src/_data/meta.js
+++ src/_data/meta.js
@@ -1,8 +1,8 @@
 module.exports = {
   url: process.env.URL || "http://localhost:8080",
-  siteName: "11ty Netlify Jumpstart",
+  siteName: "levitate",
   siteDescription:
-    "Quickly launch an 11ty-generated static site. Includes a minimal Sass framework, and generated sitemap, RSS feed, and social share preview images.",
-  authorName: "Stephanie Eckles",
-  twitterUsername: "5t3ph", // no `@`
+    "Full stack developer with microservices and Angular portfolio of past projects and current endeavors.",
+  authorName: "Lev Kim",
+  twitterUsername: "", // no `@`
 };
